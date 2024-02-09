64ce0b00dcbfad8ddd3a3233134cb5004ea6c67b
antonyjes <72809149@continental.edu.pe>
2024-02-09 09:59:17 -0500

    Navbar complete

---
+++ client/src/components/home-nav.jsx
@@ -0,0 +1,25 @@
+import { Command } from "lucide-react"
+
+export const HomeNav = () => {
+    return(
+        <div className="flex gap-6 md:gap-10">
+            <a href="/" className="hidden items-center space-x-2 md:flex">
+                <Command />
+                <span className="hidden font-bold sm:inline-block">
+                    CRUD
+                </span>
+            </a>
+            <nav className="hidden gap-6 md:flex">
+                <a href="/" className="text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm">
+                    Inicio
+                </a>
+                <a href="/pricing" className="text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm">
+                    Pricing
+                </a>
+                <a href="/aboutus" className="text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm">
+                    About us
+                </a>
+            </nav>
+        </div>
+    )
+}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { HomeNav } from "@/components/home-nav";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
@@ -6,6 +7,7 @@ const HomePage = () => {
         <div className="flex min-h-screen flex-col">
             <header className="container z-40 bg-background">
                 <div className="flex h-20 items-center justify-between py-6">
+                    <HomeNav />
                     <nav>
                         <a href="/login" className={cn(buttonVariants({variant: "secondary", size: "sm"}), "px-4")}>Login</a>
                         <a href="/register" className={cn(buttonVariants({variant: "primary", size: "sm"}), "px-4")}>Register</a>
