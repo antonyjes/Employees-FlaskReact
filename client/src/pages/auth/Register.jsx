7f1c0320a95e2d1cb9541524272ae0933a2daa0c
antonyjes <72809149@continental.edu.pe>
2024-02-10 22:29:30 -0500

    Form register

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import LayoutHome from "../LayoutHome";
 import { buttonVariants } from "@/components/ui/button";
 import { Command } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const Register = () => {
     return(
@@ -15,6 +17,46 @@ const Register = () => {
                                 <Command className="mx-auto h-6 w-6" />
                                 <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
                                 <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
+                            </div>
+                            <div className="grid gap-6">
+                                <form className="grid gap-6">
+                                    <div className="grip gap-2">
+                                        <div className="grid gap-1">
+                                            <Label htmlFor="email">
+                                                Email
+                                            </Label>
+                                            <Input
+                                                id="email"
+                                                placeholder="name@example.com"
+                                                type="email"                                                
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="grip gap-2">
+                                        <div className="grid gap-1">
+                                            <Label htmlFor="username">
+                                                Username
+                                            </Label>
+                                            <Input
+                                                id="username"
+                                                placeholder="example"
+                                                type="text"                                                
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="grip gap-2">
+                                        <div className="grid gap-1">
+                                            <Label htmlFor="password">
+                                                Password
+                                            </Label>
+                                            <Input
+                                                id="password"
+                                                placeholder="..."
+                                                type="password"                                                
+                                            />
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
