02bd63ca74f043ff868bb585b2a552dd471129b4
antonyjes <72809149@continental.edu.pe>
2024-02-12 13:21:37 -0500

    Test flask and mysql connect

--- server/app.py
+++ server/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crud_db'
+mysql = MySQL(app)
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello world</p>"
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+    cursor.close()
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
