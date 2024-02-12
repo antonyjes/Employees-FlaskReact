1fd3ae8522d998945b5205b56976636e1a2d8dd2
antonyjes <72809149@continental.edu.pe>
2024-02-12 16:52:03 -0500

    Function to save user into db

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -9,14 +12,22 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'crud_db'
 mysql = MySQL(app)
 
-@app.route("/")
-def hello_world():
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM users")
-    result = cursor.fetchall()
-    cursor.close()
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
 
-    return jsonify(result)
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)", (username, email, generate_password_hash(password)))
+        mysql.connection.commit()
+        cursor.close()
+        return jsonify({'messsage': 'Success'}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
