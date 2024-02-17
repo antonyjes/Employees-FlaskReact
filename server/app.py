ce018f80772e442be4da498b58179b99d16c6618
antonyjes <72809149@continental.edu.pe>
2024-02-17 17:33:16 -0500

    API Create employee - send image

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_mysqldb import MySQL, MySQLdb
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from gentoken import generate_token
 from datetime import datetime
+from uniqueFilename import generate_unique_filename
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +17,7 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'crud_db'
 mysql = MySQL(app)
 
+app.config['UPLOAD_FOLDER'] = 'assets'
 
 @app.route("/register", methods=['POST'])
 def register():
@@ -155,6 +159,49 @@ def getEmployees(userId):
             return jsonify({"error": str(e)}), 500
     else:
         return jsonify({'error': 'Invalid token'}), 401
+    
+
+@app.route("/employees/create", methods=['POST'])
+def createEmployee():
+    data = request.form
+    firstName = data.get('firstName')
+    lastName = data.get('lastName')
+    age = data.get('age')
+    companyId = data.get('companyId')
+    image = request.files['imageReal']
+    token = request.headers.get("Authorization")
+
+    if token:
+        try:
+            if image:
+                filename = secure_filename(image.filename)
+                unique_filename = generate_unique_filename(filename)
+                upload_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+                if not os.path.exists(upload_path):
+                    os.makedirs(upload_path)
+                imagePath = os.path.join(upload_path, unique_filename)
+                with open(imagePath, 'wb') as f:
+                    image.save(f)
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("INSERT INTO employees(firstName, lastName, age, image, companyId) VALUES (%s, %s, %s, %s, %s)", (firstName, lastName, age, unique_filename, companyId,))
+            mysql.connection.commit()
+            cursor.execute("SELECT * FROM employees WHERE id = %s", (cursor.lastrowid,))
+            new_employee = cursor.fetchone()
+            cursor.close()
+            return jsonify(new_employee), 200            
+        except Exception as e:
+            app.logger.error(e)
+            return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({'error': 'Invalid token'}), 401
+
+
+@app.route("/assets/<filename>", methods=['GET'])
+def getImage(filename):
+    try:
+        return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    except FileNotFoundError:
+        return jsonify({'error': 'Image not found'}), 404
 
 
 if __name__ == "__main__":
